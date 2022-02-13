8e6f498ce69221b669f071af183c841f016a6880
mohanson <mohanson@outlook.com>
2022-02-14 00:24:14 +0800

    2022-02-14 00:24:14

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,45 @@
-pub trait Eint: Clone + Copy + From<bool> {
+pub trait Eint: Clone + Copy + Eq + PartialEq + std::fmt::Debug {
+    const BITS: u32;
     const MIN_U: Self;
     const MAX_U: Self;
     fn overflowing_add_u(self, other: Self) -> (Self, bool);
 }
 
 #[macro_export]
+macro_rules! construct_eint_wrap_from_uint {
+    ($name:ident, $uint:ty, $from:ty) => {
+        impl std::convert::From<$from> for $name {
+            fn from(small: $from) -> Self {
+                Self(small as $uint)
+            }
+        }
+    };
+}
+
+#[macro_export]
 macro_rules! construct_eint_wrap {
     ($name:ident, $uint:ty, $sint:ty) => {
-        #[derive(Clone, Copy)]
+        #[derive(Clone, Copy, Eq, PartialEq)]
         pub struct $name(pub $uint);
 
-        impl std::convert::From<bool> for $name {
-            fn from(small: bool) -> Self {
-                Self(small as $uint)
+        construct_eint_wrap_from_uint!($name, $uint, bool);
+        construct_eint_wrap_from_uint!($name, $uint, u8);
+        construct_eint_wrap_from_uint!($name, $uint, u16);
+        construct_eint_wrap_from_uint!($name, $uint, u32);
+        construct_eint_wrap_from_uint!($name, $uint, u64);
+        construct_eint_wrap_from_uint!($name, $uint, u128);
+
+        impl std::fmt::Debug for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let suffix = format!("{:x}", self.0);
+                let prefix = String::from("0").repeat(Self::BITS as usize / 4 - suffix.len());
+                write!(f, "{}", prefix)?;
+                write!(f, "{}", suffix)
             }
         }
 
         impl Eint for $name {
+            const BITS: u32 = <$uint>::BITS;
             const MIN_U: Self = Self(0);
             const MAX_U: Self = Self(<$uint>::MAX);
 
@@ -29,18 +52,38 @@ macro_rules! construct_eint_wrap {
 }
 
 #[macro_export]
+macro_rules! construct_eint_twin_from_uint {
+    ($name:ident, $half:ty, $from:ty) => {
+        impl std::convert::From<$from> for $name {
+            fn from(small: $from) -> Self {
+                debug_assert!($name::BITS >= 256);
+                Self(<$half>::from(small), <$half>::MIN_U)
+            }
+        }
+    };
+}
+
+#[macro_export]
 macro_rules! construct_eint_twin {
     ($name:ident, $half:ty) => {
-        #[derive(Clone, Copy)]
+        #[derive(Clone, Copy, Eq, PartialEq)]
         pub struct $name(pub $half, pub $half);
 
-        impl std::convert::From<bool> for $name {
-            fn from(small: bool) -> Self {
-                Self(<$half>::from(small), <$half>::MIN_U)
+        construct_eint_twin_from_uint!($name, $half, bool);
+        construct_eint_twin_from_uint!($name, $half, u8);
+        construct_eint_twin_from_uint!($name, $half, u16);
+        construct_eint_twin_from_uint!($name, $half, u32);
+        construct_eint_twin_from_uint!($name, $half, u64);
+        construct_eint_twin_from_uint!($name, $half, u128);
+
+        impl std::fmt::Debug for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{:?}{:?}", self.1, self.0)
             }
         }
 
         impl Eint for $name {
+            const BITS: u32 = <$half>::BITS * 2;
             const MIN_U: Self = Self(<$half>::MIN_U, <$half>::MIN_U);
             const MAX_U: Self = Self(<$half>::MAX_U, <$half>::MAX_U);
 
@@ -59,7 +102,6 @@ construct_eint_wrap!(E16, u16, i16);
 construct_eint_wrap!(E32, u32, i32);
 construct_eint_wrap!(E64, u64, i64);
 construct_eint_wrap!(E128, u128, i128);
-construct_eint_twin!(T128, E64);
 construct_eint_twin!(E256, E128);
 construct_eint_twin!(E512, E256);
 construct_eint_twin!(E1024, E512);
--- tests/proptest.rs
+++ tests/proptest.rs
@@ -1,8 +1,24 @@
+use eint::*;
 use proptest::prelude::*;
+
+construct_eint_twin!(T64, E32);
+
+impl T64 {
+    fn recv(small: u64) -> Self {
+        Self(E32(small as u32), E32((small >> 32) as u32))
+    }
+
+    fn into(self) -> E64 {
+        E64(((self.1 .0 as u64) << 32) | (self.0 .0 as u64))
+    }
+}
 
 proptest! {
     #[test]
-    fn test_overflowing_add_u(x in u128::MIN..=u128::MAX, y in u128::MIN..=u128::MAX) {
-        let e0 = E128::from(x)
+    fn test_overflowing_add_u(x in u64::MIN..=u64::MAX, y in u64::MIN..=u64::MAX) {
+        let e0 = Eint::overflowing_add_u(E64::from(x), E64::from(y));
+        let e1 = Eint::overflowing_add_u(T64::recv(x), T64::recv(y));
+        assert_eq!(e0.0, e1.0.into());
+        assert_eq!(e0.1, e1.1);
     }
 }
