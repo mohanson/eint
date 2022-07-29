994c0087c2c2af5e1e91f6282f68ee25d710a561
Mohanson <mohanson@outlook.com>
2022-07-29 12:33:04 +0800

    Refactor

--- src/lib.c
+++ src/lib.c
@@ -6,7 +6,7 @@ typedef __int128_t int128_t;
 
 // HAC, 14.12 Algorithm Multiple-precision multiplication
 // SEW-bit*SEW-bit multiply to generate a 2*SEW-bit product
-void eint_widening_mul(uint64_t *w, uint64_t *x, uint64_t *y,
+void c_widening_mul_u(uint64_t *w, uint64_t *x, uint64_t *y,
                        size_t digits_count) {
   for (size_t i = 0; i < 2 * digits_count; i++) {
     w[i] = 0;
@@ -23,7 +23,7 @@ void eint_widening_mul(uint64_t *w, uint64_t *x, uint64_t *y,
 }
 
 // SEW-bit*SEW-bit multiply to generate a SEW-bit product
-void eint_mul(uint64_t *w, uint64_t *x, uint64_t *y, size_t digits_count) {
+void c_wrapping_mul(uint64_t *w, uint64_t *x, uint64_t *y, size_t digits_count) {
   for (size_t i = 0; i < digits_count; i++) {
     w[i] = 0;
   }
@@ -42,7 +42,7 @@ void eint_mul(uint64_t *w, uint64_t *x, uint64_t *y, size_t digits_count) {
 }
 
 // SEW-bit*SEW-bit addition to generate a SEW-bit result with overflowing flag
-uint64_t eint_add(uint64_t *w, uint64_t *x, uint64_t *y, size_t digits_count) {
+uint64_t c_overflowing_add(uint64_t *w, uint64_t *x, uint64_t *y, size_t digits_count) {
   uint64_t c = 0;
   for (size_t i = 0; i < digits_count; i++) {
     uint128_t temp = (uint128_t)x[i] + (uint128_t)y[i] + c;
@@ -53,7 +53,7 @@ uint64_t eint_add(uint64_t *w, uint64_t *x, uint64_t *y, size_t digits_count) {
 }
 
 // SEW-bit*SEW-bit subtraction to generate a SEW-bit result with borrowing flag
-uint64_t eint_sub(uint64_t *w, uint64_t *x, uint64_t *y, size_t digits_count) {
+uint64_t c_overflowing_sub(uint64_t *w, uint64_t *x, uint64_t *y, size_t digits_count) {
   int64_t c = 0;
   for (size_t i = 0; i < digits_count; i++) {
     int128_t temp = (int128_t)x[i] - (int128_t)y[i] + c;
@@ -65,76 +65,76 @@ uint64_t eint_sub(uint64_t *w, uint64_t *x, uint64_t *y, size_t digits_count) {
 
 // we should specify digits_count in C code to enable optimization from
 // compiler. so don't export function eint_widening_mul to Rust code directly.
-void eint_widening_mul_1024_batch(uint64_t *w, uint64_t *x, uint64_t *y,
+void c_widening_mul_u_1024_batch(uint64_t *w, uint64_t *x, uint64_t *y,
                                   size_t batch) {
   for (size_t i = 0; i < batch; i++) {
-    eint_widening_mul(w, x, y, 16);
+    c_widening_mul_u(w, x, y, 16);
     w += 32;
     x += 16;
     y += 16;
   }
 }
 
-void eint_widening_mul_512_batch(uint64_t *w, uint64_t *x, uint64_t *y,
+void c_widening_mul_u_512_batch(uint64_t *w, uint64_t *x, uint64_t *y,
                                  size_t batch) {
   for (size_t i = 0; i < batch; i++) {
-    eint_widening_mul(w, x, y, 8);
+    c_widening_mul_u(w, x, y, 8);
     w += 16;
     x += 8;
     y += 8;
   }
 }
 
-void eint_widening_mul_256_batch(uint64_t *w, uint64_t *x, uint64_t *y,
+void c_widening_mul_u_256_batch(uint64_t *w, uint64_t *x, uint64_t *y,
                                  size_t batch) {
   for (size_t i = 0; i < batch; i++) {
-    eint_widening_mul(w, x, y, 4);
+    c_widening_mul_u(w, x, y, 4);
     w += 8;
     x += 4;
     y += 4;
   }
 }
 
-void eint_widening_mul_128_batch(uint64_t *w, uint64_t *x, uint64_t *y,
+void c_widening_mul_u_128_batch(uint64_t *w, uint64_t *x, uint64_t *y,
                                  size_t batch) {
   for (size_t i = 0; i < batch; i++) {
-    eint_widening_mul(w, x, y, 2);
+    c_widening_mul_u(w, x, y, 2);
     w += 4;
     x += 2;
     y += 2;
   }
 }
 
-void eint_mul_1024_batch(uint64_t *w, uint64_t *x, uint64_t *y, size_t batch) {
+void c_wrapping_mul_1024_batch(uint64_t *w, uint64_t *x, uint64_t *y, size_t batch) {
   for (size_t i = 0; i < batch; i++) {
-    eint_mul(w, x, y, 16);
+    c_wrapping_mul(w, x, y, 16);
     w += 16;
     x += 16;
     y += 16;
   }
 }
 
-void eint_mul_512_batch(uint64_t *w, uint64_t *x, uint64_t *y, size_t batch) {
+void c_wrapping_mul_512_batch(uint64_t *w, uint64_t *x, uint64_t *y, size_t batch) {
   for (size_t i = 0; i < batch; i++) {
-    eint_mul(w, x, y, 8);
+    c_wrapping_mul(w, x, y, 8);
     w += 8;
     x += 8;
     y += 8;
   }
 }
 
-void eint_mul_256_batch(uint64_t *w, uint64_t *x, uint64_t *y, size_t batch) {
+void c_wrapping_mul_256_batch(uint64_t *w, uint64_t *x, uint64_t *y, size_t batch) {
   for (size_t i = 0; i < batch; i++) {
-    eint_mul(w, x, y, 4);
+    c_wrapping_mul(w, x, y, 4);
     w += 4;
     x += 4;
     y += 4;
   }
 }
 
-void eint_mul_128_batch(uint64_t *w, uint64_t *x, uint64_t *y, size_t batch) {
+void c_wrapping_mul_128_batch(uint64_t *w, uint64_t *x, uint64_t *y, size_t batch) {
   for (size_t i = 0; i < batch; i++) {
-    eint_mul(w, x, y, 2);
+    c_wrapping_mul(w, x, y, 2);
     w += 2;
     x += 2;
     y += 2;
